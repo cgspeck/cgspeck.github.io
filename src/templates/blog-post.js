c74cb71c557f7ddcd299fea6f6da8e0307d4ce2f
Chris Speck <cgspeck@gmail.com>
2023-07-31 19:00:51 +1000

    Fix build warnings

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,13 +2,13 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <Seo title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <i>{post.frontmatter.date}</i>
