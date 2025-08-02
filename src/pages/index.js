b580c1dea873a64217da3ad74ab204ed85a26032
Chris Speck <cgspeck@gmail.com>
2025-08-02 15:35:14 +1000

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,22 +15,54 @@ export default function Home() {
       <p>You may be interested in the following tools I've published:</p>
       <ul>
         <li>
+          A Windows tool written in Python/TkInter helps you create templates
+          for the Behringer X-Touch Mini and Microsoft Flight Simulator
+          <br />- my tool:{" "}
+          <ExternalLink url="https://github.com/cgspeck/X-Touch-Mini-FS2020-TemplateMaker/tree/main">
+            X-Touch-Mini-FS2020-TemplateMaker
+          </ExternalLink>
+          <br />- my templates for FBW{" "}
+          <ExternalLink url="https://flybywiresim.com/">A20N</ExternalLink> &
+          PMDG{" "}
+          <ExternalLink url="https://pmdg.com/pmdg-737-800-for-microsoft-flight-simulator-2020/">
+            738
+          </ExternalLink>
+          :{" "}
+          <ExternalLink url="https://github.com/cgspeck/x-touch-fs2020-configs">
+            x-touch-fs2020-configs
+          </ExternalLink>
+        </li>
+        <li>
+          Some Linux command line tools written in <ExternalLink url="https://www.rust-lang.org/">Rust</ExternalLink>:
+          <br /> -{" "}
+          <ExternalLink url="https://github.com/cgspeck/yapc">
+            Yet Another Pushover Client
+          </ExternalLink>
+          : send messages and hook into shell to report status of long-running
+          commands
+          <br /> - <ExternalLink url="https://github.com/cgspeck/wakenrun">Wake-N-Run</ExternalLink>: Wake-On-Lan tool to wake up remote machines, run commands on them via ssh and shut them down when finished
+          <br /> - <ExternalLink url="https://github.com/cgspeck/gen-systemd-svcs">Generate Systemd Services</ExternalLink>: generate many systemd service files given easy to use yaml template
+        </li>
+        <li>
           Photographers'{" "}
           <ExternalLink url="https://matcalculator.chrisspeck.com/">
             Mat Board Calculator
-          </ExternalLink>
+          </ExternalLink>{" "}
+          web app
         </li>
         <li>
           Homebrewers'{" "}
           <ExternalLink url="https://hopscalculator.chrisspeck.com/">
             Hop Aging Calculator
-          </ExternalLink>
+          </ExternalLink>{" "}
+          web app
         </li>
         <li>
-          Multi-computer{" "}
+          Multi-computer Linux/Windows{" "}
           <ExternalLink url="https://github.com/cgspeck/fg-orchestrator">
             launcher for FlighGear
-          </ExternalLink>
+          </ExternalLink>{" "}
+          written in Python with QT
         </li>
         <li>
           A 3d printable{" "}
@@ -41,16 +73,6 @@ export default function Home() {
           <ExternalLink url="https://www.thingiverse.com/thing:3021944">
             parametric cat food lid
           </ExternalLink>
-        </li>
-      </ul>
-      <p>Some interesting projects I'm working on that are in progress:</p>
-      <ul>
-        <li>
-          a wifi-controllable{" "}
-          <ExternalLink url="https://github.com/cgspeck/barndoor-tracker-pi">
-            Raspberry Pi + Arduino astro-photography tracker
-          </ExternalLink>{" "}
-          with align-assist, dew controller and intervalometer
         </li>
       </ul>
       <p>If you wish to contact me, my email address is cgspeck at gmail.com</p>
